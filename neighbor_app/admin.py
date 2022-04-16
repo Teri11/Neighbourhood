6e1b20ed5ab73db44eeeea41c23cdea37b77ed34
Teri11 <augustineteri157@gmail.com>
2022-04-16 08:23:17 +0300

    added registration admin

--- neighbor_app/admin.py
+++ neighbor_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Business, NeighborHood, Post, Profile
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(NeighborHood)
