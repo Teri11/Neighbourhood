00650769d9749ec48ba5740a11ef1a2b5120be4a
Teri11 <augustineteri157@gmail.com>
2022-04-16 07:41:24 +0300

    added imports

--- neighbor_app/views.py
+++ neighbor_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_text
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from .tokens import account_activation_token
+from django.template.loader import render_to_string
+from .forms import SignUpForm
+from .tokens import account_activation_token
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.core.mail import EmailMessage
+from .models import Profile,NeighborHood,Post,Business
+from .forms import *
 
-# Create your views here.
