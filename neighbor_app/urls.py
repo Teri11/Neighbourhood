46b91ef50feccea09ca942e07bf2f6b0bb881953
Teri11 <augustineteri157@gmail.com>
2022-04-19 07:43:13 +0300

    bulk commit

--- neighbor_app/urls.py
+++ neighbor_app/urls.py
@@ -4,8 +4,6 @@ from django.conf.urls import url
 from . import views as app_views
 from django.contrib.auth import views as auth_views
 
-
-
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('', app_views.index, name="home"),
